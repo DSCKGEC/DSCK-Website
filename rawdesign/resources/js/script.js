0210ef54e6added89ea38e4ce243dcaa4facca95
Abhinaba <abhinaba006@gmail.com>
2020-12-06 03:14:00 -0700

    particale effect added

--- rawdesign/resources/js/script.js
+++ rawdesign/resources/js/script.js
@@ -1,5 +1,5 @@
 // *** TO BE CUSTOMISED ***
-
+//<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 var style_cookie_name = "style";
 var style_cookie_duration = 30;
 var style_domain = "dsckgec.ml";
