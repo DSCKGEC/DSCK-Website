fb1e386e3e292ea2a68d3357aa01b3fb6904173d
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-28 20:53:37 +0530

    Added blank files

---
+++ frontend/src/Containers/Layout/Layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+
+const Layout:React.FC = () => {
+  return (
+      <Navbar></Navbar>
+  );
+}
+
+export default Layout;
