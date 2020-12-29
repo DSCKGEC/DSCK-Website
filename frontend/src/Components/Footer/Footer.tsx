f38b49fdda9b4527095841ac44c8f5572d75fe14
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-29 19:49:57 +0530

    Init sections

---
+++ frontend/src/Components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer:React.FC = () => {
+  return (null);
+}
+
+export default Footer;
---
+++ frontend/src/Containers/Projects/Projects.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Projects.scss';
+
+const Projects:React.FC = () => {
+  return (null);
+}
+
+export default Projects;
