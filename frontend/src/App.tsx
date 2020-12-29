0160904f65025209a9ecbe3bd7493e516f086e88
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-29 19:48:59 +0530

    Added Layout imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Layout from './Containers/Layout/Layout';
 
 function App() {
   return (
     <div className="main">
+      <Layout/>
     </div>
   );
 }
