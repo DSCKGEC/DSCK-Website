9434159a1c3676c7479dcb8c43384d7916f2b323
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-30 22:15:45 +0530

    Added socials hrefs

--- frontend/src/Components/Socials/Socials.tsx
+++ frontend/src/Components/Socials/Socials.tsx
@@ -12,20 +12,20 @@ const Socials: React.FC<Props> = (props) => {
         <div className="scrolled"  style={{height:`${props.percent}%`}}></div>
       </div>
       <div className="socials">
-        <a href="">
+        <a href="https://facebook.com/dsckgec">
           <i className="fa fa-facebook"></i>
         </a>
-        <a href="">
+        <a href="https://instagram.com/dsckgec">
           <i className="fa fa-instagram"></i>
         </a>
-        <a href="">
+        <a href="https://www.youtube.com/channel/UChIuhjS8Kny2wc7mZrVjSuQ">
           <i className="fa fa-youtube"></i>
         </a>
-        <a href="">
+        <a href="https://twitter.com/dsckgec">
           <i className="fa fa-twitter"></i>
         </a>
-        <a href="">
-          <i className="fa fa-comments"></i>
+        <a href="https://www.linkedin.com/company/dsckgec">
+          <i className="fa fa-linkedin"></i>
         </a>
       </div>
     </div>
