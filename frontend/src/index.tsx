0365590ce5cfa7f583437b85552c024c87cb675e
KylixMedusa <ayush.agarwalmng@gmail.com>
2021-04-09 18:42:55 +0530

    Added scroll to top

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,11 +4,14 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
